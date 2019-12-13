011f13bce13bc2299838491ea8514f8525baf7e0
jfk17a <44479985+jfk17a@users.noreply.github.com>
2019-12-12 19:01:52 -0600

    Initial commit

---
+++ app/src/main/java/com/example/gumdropv02/SantaListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gumdropv02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SantaListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_santa_list)
+    }
+}
